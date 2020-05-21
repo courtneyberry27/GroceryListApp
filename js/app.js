ac82aaf5474ef2b00814a7904134234bccc892f4
Courtney Berry <64494409+courtneyberry27@users.noreply.github.com>
2020-05-21 11:20:55 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,79 +10,79 @@ const removeItemButton = document.querySelector('button.removeItemButton');
 const list = listUl.children;
 
 function attachListButtons(li) {
-    let up = document.createElement('button');
-    up.className = 'up';
-    up.textContent = 'up';
-    li.appendChild(up);
-
-    let down = document.createElement('button');
-    down.className = 'down';
-    down.textContent = 'down';
-    li.appendChild(down);
-
-    let remove = document.createElement('button');
-    remove.className = 'remove';
-    remove.textContent = 'remove';
-    li.appendChild(remove);
+  let up = document.createElement('button');
+  up.className = 'up';
+  up.textContent = 'up';
+  li.appendChild(up);
+  
+  let down = document.createElement('button');
+  down.className = 'down';
+  down.textContent = 'down';
+  li.appendChild(down);
+  
+  let remove = document.createElement('button');
+  remove.className = 'remove';
+  remove.textContent = 'remove';
+  li.appendChild(remove);
 }
 
 for (let i = 0; i < list.length; i += 1) {
-    attachListButtons(list[i]);
+  attachListButtons(list[i]);
 }
 
 listUl.addEventListener('click', (event) => {
-    if (event.target.tagName == 'BUTTON') {
-        if (event.target.className == 'remove') {
-            let li = event.target.parentNode;
-            let ul = li.parentNode;
-            ul.removeChild(li);
-        }
-        if (event.target.className == 'up') {
-            let li = event.target.parentNode;
-            let prevLi = li.previousElementSibling;
-            let ul = li.parentNode;
-            if (prevLi) {
-                ul.insertBefore(li, prevLi);
-            }
-        }
-        if (event.target.className == 'down') {
-            let li = event.target.parentNode;
-            let nextLi = li.nextElementSibling;
-            let ul = li.parentNode;
-            if (nextLi) {
-                ul.insertBefore(nextLi, li);
-            }
-        }
+  if (event.target.tagName == 'BUTTON') {
+    if(event.target.className == 'remove') {
+    let li = event.target.parentNode;
+    let ul = li.parentNode;
+    ul.removeChild(li);
+  }
+  if (event.target.className == 'up') {
+    let li = event.target.parentNode;
+    let prevLi = li.previousElementSibling;
+    let ul = li.parentNode;
+    if (prevLi) {
+      ul.insertBefore(li, prevLi);
     }
+  }
+  if (event.target.className == 'down') {
+    let li = event.target.parentNode;
+    let nextLi = li.nextElementSibling;
+    let ul = li.parentNode;
+    if (nextLi) {
+      ul.insertBefore(nextLi, li);
+    }
+  }
+}
 });
 
 
 toggleList.addEventListener('click', () => {
-    if (listDiv.style.display == 'none') {
-        toggleList.textContent = 'Hide list';
-        listDiv.style.display = 'block';
-    } else {
-        toggleList.textContent = "Show List";
-        listDiv.style.display = 'none';
-    }
+  if (listDiv.style.display == 'none') {
+    toggleList.textContent = 'Hide list';
+    listDiv.style.display = 'block';
+  } else {
+    toggleList.textContent = "Show List";
+    listDiv.style.display = 'none';
+  }
 });
 
 descriptionButton.addEventListener('click', () => {
-    descriptionP.innerHTML = descriptionInput.value + ':';
-    descriptionInput.value = '';
+  descriptionP.innerHTML = descriptionInput.value + ':';
+  descriptionInput.value = '';
 });
 
 addItemButton.addEventListener('click', () => {
-    let ul = document.getElementsByTagName('ul')[0];
-    let li = document.createElement('li');
-    li.textContent = addItemInput.value;
-    attachListButtons(li);
-    ul.appendChild(li);
-    addItemInput.value = '';
+  let ul = document.getElementsByTagName('ul')[0];
+  let li = document.createElement('li');
+  li.textContent = addItemInput.value;
+  attachListButtons(li);
+  ul.appendChild(li);
+  addItemInput.value = '';
 });
 
 removeItemButton.addEventListener('click', () => {
-    let ul = document.getElementsByTagName('ul')[0];
-    let li = document.querySelector('li:last-child');
-    ul.removeChild(li);
+  let ul = document.getElementsByTagName('ul')[0];
+  let li = document.querySelector('li:last-child');
+  ul.removeChild(li);
 });
